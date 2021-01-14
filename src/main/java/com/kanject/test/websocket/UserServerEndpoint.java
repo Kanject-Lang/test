58e77bc746abcfd51f757af6501bc870134ae7f9
guangjie.liang <guangjie.liang@unionman.com.cn>
2021-01-14 14:23:55 +0800

    websocket聊天室--广播消息、艾特用户、用户上下线广播功能实现

--- src/main/java/com/kanject/test/websocket/UserServerEndpoint.java
+++ src/main/java/com/kanject/test/websocket/UserServerEndpoint.java
@@ -8,6 +8,12 @@ import javax.websocket.*;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * TODO
@@ -20,41 +26,61 @@ import java.io.IOException;
 @ServerEndpoint("/ws/user/{username}")//把当前类标识成一个WebSocket的服务端
 public class UserServerEndpoint {
 
-    /** 保存用户会话，实例变量，每个用户一个 */
+    /** 用户在线数 */
+    private static int onlineCount;
+
+    /** 用户-websocket服务端 Map */
+    private static Map<String, UserServerEndpoint> userMap = new ConcurrentHashMap<>();
+
+//    /** 用户名set，艾特用户的时候用到 */
+//    private static List<String> atUsernameList = new CopyOnWriteArrayList<>();
+
+    /** 记录用户会话，实例变量，每个用户一个 */
     private Session session;
 
+    /** 记录用户名，实例变量，每个用户一个 */
+    private String username;
+
+    /**
+     * 连接建立成功调用的方法
+     */
     @OnOpen
     public void onOpen(Session session, @PathParam("username") String username) {
         this.session = session;
-        try {
-            session.getBasicRemote().sendText(String.format("[%s]进入了聊天室", username));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        log.info(String.format("[%s]进入了聊天室", username));
-    }
-
-    @OnClose
-    public void onClose(Session session, @PathParam("username") String username) {
-        this.session = session;
-        try {
-            session.getBasicRemote().sendText(String.format("[%s]退出了聊天室", username));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        log.info(String.format("[%s]退出了聊天室", username));
+        this.username = username;
+        userMap.put("@" + this.username, this);
+//        atUsernameList.add("@" + this.username);
+        onlineCountIncrease();
+        broadcast(String.format("[%s]进入了聊天室, 当前聊天室人数为[%d]", this.username, getOnlineCount()));
+        log.info(String.format("[%s]进入了聊天室, 当前聊天室人数为[%d]", this.username, getOnlineCount()));
+        log.info("userMap =====> {}", userMap);
     }
 
     /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        userMap.remove("@" + this.username);
+//        atUsernameList.remove("@" + this.username);
+        onlineCountDecrease();
+        broadcast(String.format("[%s]退出了聊天室, 当前聊天室人数为[%d]", this.username, getOnlineCount()));
+        log.info(String.format("[%s]退出了聊天室, 当前聊天室人数为[%d]", this.username, getOnlineCount()));
+        log.info("userMap =====> {}", userMap);
+    }
+
+    /**
+     * 发生错误时调用
      * OnError方法必须要有Throwable参数，否则抛：
      *  javax.websocket.DeploymentException: No Throwable parameter was present on the method [onError] of class [..TestServerEndpoint] that was annotated with OnError
      */
     @OnError
     public void onError(Throwable error) {
-        log.info("TestServerEndpoint is on error =====> {}", error.getMessage());
+        log.error("TestServerEndpoint is on error =====> {}", error.getMessage());
     }
 
-    /** @OnMessage 方法必须要有参数
+    /** 收到客户端消息后调用的方法
+     *  @OnMessage 方法必须要有参数
      *  否则提示： @OnMessage method must have parameters */
     @OnMessage
     public void onMessage(String message) {
@@ -62,6 +88,53 @@ public class UserServerEndpoint {
         if (message.equals("I am your Dad.")) {
             throw new RuntimeException("Dad, I am sorry.");
         }
+
+        if (!message.contains("@")) {//消息文本中不含'@'，直接广播消息
+            broadcast(message);
+        } else {//消息文本中包含'@'，执行以下逻辑
+            boolean broadcastFlag = true;
+            for (String atUsername : userMap.keySet()) {
+                if (message.contains(atUsername)) {
+                    broadcastFlag = false;
+                    log.info("@ user =====> [{}]", atUsername.substring(1));
+//                    userMap.get(atUsername.substring(1)).sendMessage(message);
+                    userMap.get(atUsername).sendMessage(message);
+                }
+            }
+            if (broadcastFlag) {
+                broadcast(message);
+            }
+        }
     }
+
+    /**
+     * 广播消息
+     */
+    private void broadcast(String message) {
+        for (String key : userMap.keySet()) {
+            userMap.get(key).sendMessage(message);
+        }
+    }
+
+    private void sendMessage(String message) {
+        try {
+            this.session.getBasicRemote().sendText(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static synchronized int getOnlineCount() {
+        return onlineCount;
+    }
+
+    private static synchronized void onlineCountIncrease() {
+        onlineCount++;
+    }
+
+    private static synchronized void onlineCountDecrease() {
+        onlineCount--;
+    }
+
 
 }
